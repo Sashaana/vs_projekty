de6eebb9abaa86dfad8e9a222bc118adef912720
Sashaana <tukac.dominik@gmail.com>
2024-09-12 11:53:00 +0200

    test1

--- skript1.py
+++ skript1.py
@@ -2,8 +2,17 @@ print("ahoj")
 
 list = [1, 2, 3, 4, 5]
 
+dictionary = {"one": 1}
+
 print("Hello World")
 
-print("přidal jsem další print")
+print("provedli jsme zmeny")
+
+print("pridal jsem dalsi print")
+
+
+
+
+
 
 
